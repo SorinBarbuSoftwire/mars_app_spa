802feec75ef97b902aa8531678662d0987ad1035
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-07-03 12:58:34 +0300

    code reforamt

--- src/App.tsx
+++ src/App.tsx
@@ -4,27 +4,20 @@ import './App.css';
 import NASA_info from "./NASA_info";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {NASA_info(
-            "NASA",
-            "https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg",
-            "The National Aeronautics and Space Administration is America's civil space program and the global leader in space exploration.",
-            "The agency has a diverse workforce of just under 18,000 civil servants, and works with many more U.S."
-        )}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                {NASA_info(
+                    "NASA",
+                    "https://upload.wikimedia.org/wikipedia/commons/e/e5/NASA_logo.svg",
+                    "The National Aeronautics and Space Administration is America's civil space program and the global leader in space exploration.",
+                    "The agency has a diverse workforce of just under 18,000 civil servants, and works with many more U.S."
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
