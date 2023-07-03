dbe8ed8d4789183cf98fee0ab620152f9f7fbec1
Sorin Barbu <Sorin.Barbu@sofwire.com>
2023-07-03 13:11:29 +0300

    Done 2.3

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
+import {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 import NASA_info from "./NASA_info";
 
 function App() {
-
     return (
         <div className="App">
             <header className="App-header">
@@ -15,7 +15,25 @@ function App() {
                     "The National Aeronautics and Space Administration is America's civil space program and the global leader in space exploration.",
                     "The agency has a diverse workforce of just under 18,000 civil servants, and works with many more U.S."
                 )}
+                {Counter()}
             </header>
+        </div>
+    );
+}
+
+function Counter() {
+    const [noClicks, setNoClicks] = useState(0);
+
+    function handleClick() {
+        setNoClicks(noClicks + 1)
+    }
+
+    return (
+        <div>
+            <div>{noClicks}</div>
+            <button onClick={handleClick}>
+                +
+            </button>
         </div>
     );
 }
